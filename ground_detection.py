8ec411722f92fce717c7574f107f30d5e53d7ed2
DeastinY <polzin.richard@gmail.com>
2017-01-11 18:25:27 +0100

    formatting

--- ground_detection.py
+++ ground_detection.py
@@ -22,18 +22,25 @@ def initial_mean_values():
 
 
 def tarvas_geometric(raw_image):
+    # resize
     image = cv2.resize(raw_image, (0, 0), fx=0.5, fy=0.5)
+    # color thresholding
     image_hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
     green = np.uint8([[[0, 255, 0]]])
     hsv_green = cv2.cvtColor(green, cv2.COLOR_BGR2HSV)
     print(hsv_green)  # use [H-X, 100, 100] and [H+X, 255, 255] for separation
     lower_green, upper_green = np.array([40, 100, 100]), np.array([80, 255, 255])
     mask = cv2.inRange(image_hsv, lower_green, upper_green)
+    # erode and dilate
     kernel = np.ones((10, 10), np.uint8)  # magic number 10 from paper
     mask = cv2.erode(mask, kernel)
     mask = cv2.dilate(mask, kernel)
-    cv2.imshow('original', raw_image)
+    # corner detection
+    epsilon_snap = 20  # magic number 20 from paper
 
+
+    # visualize results
+    cv2.imshow('original', raw_image)
     output = cv2.bitwise_and(image, image, mask=mask)
     cv2.imshow('overlay', output)
     cv2.waitKey(0)
